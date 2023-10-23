8e81ae6e7c9fff9334482be45c920ff818b1b457
amir7990 <amirhosein7990@gmail.com>
2023-10-23 20:02:56 +0330

    commodity test added

---
+++ CA1/src/test/java/model/CommodityTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import exceptions.NotInStock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommodityTest {
+    private Commodity commodity;
+
+    @BeforeEach
+    public void setUp() {
+        commodity = new Commodity();
+        commodity.setInStock(10);
+    }
+
+    @Test
+    public void testUpdateInStock() {
+        assertDoesNotThrow(() -> commodity.updateInStock(5));
+        assertEquals(15, commodity.getInStock());
+
+        assertThrows(NotInStock.class, () -> commodity.updateInStock(-20));
+    }
+
+    @Test
+    public void testAddRate() {
+        commodity.addRate("user1", 5);
+        assertEquals(5.0, commodity.getRating());
+
+        commodity.addRate("user2", 3);
+        assertEquals(4.0, commodity.getRating());
+    }
+}
